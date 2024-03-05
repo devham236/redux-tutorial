56702545e242dbb87733a4c13d0ea263c91b6ea8
Hamza Mirza <hamza665@hotmail.de>
2024-03-05 17:12:56 +0100

    clean up

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,9 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App.jsx"
 import "./index.css"
-import { store } from "./State/store.js"
-import { Provider } from "react-redux"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Provider>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 )
