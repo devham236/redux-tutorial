5d05839e9477bf2de5d4824be6882d195fb23d34
Hamza Mirza <hamza665@hotmail.de>
2024-03-06 16:36:23 +0100

    useDispatch

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,10 +1,19 @@
 import React, { useState } from "react"
 import { useDispatch } from "react-redux"
+import { login } from "../features/user"
 
 const Login = () => {
+  const dispatch = useDispatch()
   return (
     <div>
-      <button className=" bg-slate-400">Login</button>
+      <button
+        onClick={() =>
+          dispatch(login({ name: "Hamza", age: 25, email: "hamza@email.com" }))
+        }
+        className=" bg-slate-400"
+      >
+        Login
+      </button>
     </div>
   )
 }
