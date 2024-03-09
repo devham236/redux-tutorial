e1b9b80875795c1e7ae69d97a9d0e6fc540583fa
Hamza Mirza <hamza665@hotmail.de>
2024-03-09 15:29:09 +0100

    basic structure and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import React from "react"
+import "./App.css"
 
 const App = () => {
-  return <div></div>
+  return (
+    <div className="App">
+      {" "}
+      <div className="addUser">
+        <input type="text" placeholder="Name..." />
+        <input type="text" placeholder="Username..." />
+        <button>Add User</button>
+      </div>
+      <div className="displayUsers"></div>
+    </div>
+  )
 }
 
 export default App
