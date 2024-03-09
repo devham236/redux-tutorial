1c1f60e22c110edfc3dcdac123476144f491ef82
Hamza Mirza <hamza665@hotmail.de>
2024-03-09 13:52:24 +0100

    clean up

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,26 +1,7 @@
-import React, { useState } from "react"
-import { useDispatch } from "react-redux"
-import { login, logout } from "../features/user"
-import { toggle } from "../features/darkMode"
+import React from "react"
 
 const Login = () => {
-  const dispatch = useDispatch()
-  return (
-    <div>
-      <button
-        onClick={() =>
-          dispatch(login({ name: "Hamza", age: 25, email: "hamza@email.com" }))
-        }
-        className=" bg-slate-400"
-      >
-        Login
-      </button>
-      <button onClick={() => dispatch(logout())} className=" bg-slate-400">
-        Logout
-      </button>
-      <button onClick={() => dispatch(toggle())}>Toggle Darkmode</button>
-    </div>
-  )
+  return <div></div>
 }
 
 export default Login
