1f40e7d65985ef0671557cf3a83cff0bfdf4178b
Hamza Mirza <hamza665@hotmail.de>
2024-03-09 15:51:47 +0100

    onChange functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import "./App.css"
 import { useDispatch, useSelector } from "react-redux"
 import { addUser } from "./Features/users"
@@ -6,13 +6,38 @@ import { addUser } from "./Features/users"
 const App = () => {
   const usersList = useSelector((state) => state.users.value)
   const dispatch = useDispatch()
+  const [newUser, setNewUser] = useState({
+    id: 0,
+    name: "",
+    username: "",
+  })
+
+  const changeState = (event) => {
+    const { value, name } = event.target
+
+    setNewUser((prev) => ({
+      ...prev,
+      [name]: value,
+    }))
+  }
+
   return (
     <div className="App">
       {" "}
       <div className="addUser">
-        <input type="text" placeholder="Name..." />
-        <input type="text" placeholder="Username..." />
-        <button>Add User</button>
+        <input
+          type="text"
+          placeholder="Name..."
+          name="name"
+          onChange={(e) => changeState(e)}
+        />
+        <input
+          type="text"
+          placeholder="Username..."
+          name="username"
+          onChange={(e) => changeState(e)}
+        />
+        <button onClick={() => dispatch(addUser())}>Add User</button>
       </div>
       <div className="displayUsers">
         {usersList.map((user) => (
