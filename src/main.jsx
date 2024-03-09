ac4836ad8038eac9a4c4a852df889b1d1992137a
Hamza Mirza <hamza665@hotmail.de>
2024-03-09 15:13:53 +0100

    store created

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,17 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App.jsx"
 import "./index.css"
+import { configureStore } from "@reduxjs/toolkit"
+import { Provider } from "react-redux"
+
+const store = configureStore({
+  reducer: {},
+})
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 )
