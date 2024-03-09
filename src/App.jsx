d224408227aa85eef413bf9772a5da9ba4e04d56
Hamza Mirza <hamza665@hotmail.de>
2024-03-09 13:53:45 +0100

    clean up

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,7 @@
 import React from "react"
-import Login from "./Components/Login"
-import Profile from "./Components/Profile"
-import ChangeColor from "./Components/ChangeColor"
 
 const App = () => {
-  return (
-    <div className="flex items-center justify-center w-full h-full flex-col">
-      <Profile />
-      <Login />
-      <ChangeColor />
-    </div>
-  )
+  return <div></div>
 }
 
 export default App
