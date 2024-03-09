a1407a2426633d49f91f639e0ef2812a6660ab5b
Hamza Mirza <hamza665@hotmail.de>
2024-03-09 13:47:43 +0100

    clean up

--- src/main.jsx
+++ src/main.jsx
@@ -2,24 +2,9 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App.jsx"
 import "./index.css"
-import { configureStore } from "@reduxjs/toolkit"
-import { Provider } from "react-redux"
-import userReducer from "./features/user.js"
-import darkModeReducer from "./features/darkMode.js"
-import themeReducer from "./features/theme.js"
-
-const store = configureStore({
-  reducer: {
-    user: userReducer,
-    darkMode: darkModeReducer,
-    theme: themeReducer,
-  },
-})
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>
 )
