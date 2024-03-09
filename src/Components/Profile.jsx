20b4f2dfc81cf362d768557b084e323fddd21150
Hamza Mirza <hamza665@hotmail.de>
2024-03-09 13:51:53 +0100

    clean up

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,18 +1,7 @@
 import React from "react"
-import { useSelector } from "react-redux"
 
 const Profile = () => {
-  const user = useSelector((state) => state.user.value)
-  const theme = useSelector((state) => state.theme.value)
-
-  return (
-    <div className={``} style={{ color: theme }}>
-      <h1 className="">Profile Page</h1>
-      <p className="">Name: {user.name}</p>
-      <p className="">Age: {user.age}</p>
-      <p className="">E-Mail: {user.email}</p>
-    </div>
-  )
+  return <div></div>
 }
 
 export default Profile
