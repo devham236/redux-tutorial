ec7479ab316ce85ccddd17648597b410c888c5e9
Hamza Mirza <hamza665@hotmail.de>
2024-03-09 15:25:13 +0100

    added reducer

--- src/Features/users.js
+++ src/Features/users.js
@@ -3,4 +3,7 @@ import { createSlice } from "@reduxjs/toolkit"
 const userSlice = createSlice({
   name: "users",
   initialState: { value: [] },
+  reducers: {
+    addUser: (state, action) => {},
+  },
 })
