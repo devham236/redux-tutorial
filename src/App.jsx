c89f816abce0eea66d5582c50a970c36631751ce
Hamza Mirza <hamza665@hotmail.de>
2024-03-09 15:43:03 +0100

    added hooks to select stat and dispatch actions

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import React from "react"
 import "./App.css"
+import { useDispatch, useSelector } from "react-redux"
+import { addUser } from "./Features/users"
 
 const App = () => {
+  const usersList = useSelector((state) => state.users.value)
+  const dispatch = useDispatch()
   return (
     <div className="App">
       {" "}
@@ -10,7 +14,14 @@ const App = () => {
         <input type="text" placeholder="Username..." />
         <button>Add User</button>
       </div>
-      <div className="displayUsers"></div>
+      <div className="displayUsers">
+        {usersList.map((user) => (
+          <div key={user.id}>
+            <h1>{user.name}</h1>
+            <h1>{user.username}</h1>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
