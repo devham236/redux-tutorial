fa06a78418a585d1f02f7b089df1401f8d0a1a92
Hamza Mirza <hamza665@hotmail.de>
2024-03-16 12:23:32 +0100

    clean up

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,9 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App.jsx"
-import { configureStore } from "@reduxjs/toolkit"
-import { Provider } from "react-redux"
-import usersReducer from "./Features/users"
-
-const store = configureStore({
-  reducer: {
-    users: usersReducer,
-  },
-})
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>
 )
