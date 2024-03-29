b591969dff6578d86e385a55c484388f5683858c
Hamza Mirza <hamza665@hotmail.de>
2024-03-29 08:15:30 +0100

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import React from "react"
 
 const App = () => {
-  return <div></div>
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim odit, vero
+      fugit quis numquam ullam fuga, tenetur neque voluptate tempora dolor
+      placeat id provident cum natus perferendis nesciunt architecto est.
+    </div>
+  )
 }
 
 export default App
