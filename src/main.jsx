901ddb663303fb6c7062c8201c57497835abbe28
Hamza Mirza <hamza665@hotmail.de>
2024-04-03 17:56:04 +0200

    clean up

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App.jsx"
+
 import { configureStore } from "@reduxjs/toolkit"
 import { Provider } from "react-redux"
 import { BrowserRouter } from "react-router-dom"
+
 import recipesReducer from "./Slices/recipeSlice.js"
 import favoritesReducer from "./Slices/favoritesSlice.js"
 import modalReducer from "./Slices/modalSlice.js"
