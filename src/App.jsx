a373c9481cb05d62179bb460174c1ef4588bbab6
Hamza Mirza <hamza665@hotmail.de>
2024-04-04 16:52:30 +0200

    new page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from "react"
 import { Link, Route, Routes } from "react-router-dom"
+import { useSelector } from "react-redux"
 import Favorites from "./Pages/Favorites"
 import Recipes from "./Pages/Recipes"
-import { useSelector } from "react-redux"
 import Modal from "./Components/Modal"
+import Checkout from "./Pages/Checkout"
 
 const App = () => {
   const modal = useSelector((state) => state.modal.value)
@@ -15,13 +16,17 @@ const App = () => {
           <Link to={"/"} style={{ marginRight: "1rem" }}>
             <h1>Recipes</h1>
           </Link>
-          <Link to={"/favorites"}>
+          <Link to={"/favorites"} style={{ marginRight: "1rem" }}>
             <h1>Favorites</h1>
+          </Link>
+          <Link to={"/checkout"}>
+            <h1>Checkout</h1>
           </Link>
         </div>
         <Routes>
           <Route path="/" element={<Recipes />}></Route>
           <Route path="/favorites" element={<Favorites />}></Route>
+          <Route path="/checkout" element={<Checkout />}></Route>
         </Routes>
       </div>
       {modal && <Modal recipe={modal} />}
