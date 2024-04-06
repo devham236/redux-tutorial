fa2c337e23e4720d12e86b84da58ba5e01797b8a
Hamza Mirza <hamza665@hotmail.de>
2024-04-06 16:47:52 +0200

    filter array when input gets checked

--- src/Pages/Recipes.jsx
+++ src/Pages/Recipes.jsx
@@ -7,10 +7,13 @@ const Recipes = () => {
   const dispatch = useDispatch()
   const recipes = useSelector((state) => state.recipes.value)
   const onlyEasy = useSelector((state) => state.onlyEasy.value)
+  const recipesArray = onlyEasy
+    ? recipes.filter((recipe) => recipe.difficulty === "Easy")
+    : recipes
 
   return (
     <div>
-      {recipes.map((recipe) => (
+      {recipesArray.map((recipe) => (
         <RecipeDiv key={recipe.id} recipe={recipe} />
       ))}
       <button
