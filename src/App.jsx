ef0c5ae169e38be72139aa7fa2ee5e0189ff82b1
Hamza Mirza <hamza665@hotmail.de>
2024-04-13 12:42:50 +0200

    clean up

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,7 @@
 import React from "react"
-import { Link, Route, Routes } from "react-router-dom"
-import Recipes from "./Pages/Recipes"
-import Modal from "./Components/Modal"
-import Checkout from "./Pages/Checkout"
-import { useSelector } from "react-redux"
 
 const App = () => {
-  const modal = useSelector((state) => state.modal.value)
-
-  return (
-    <>
-      <div style={{ position: "relative" }}>
-        <div style={{ display: "flex" }}>
-          <Link to={"/"} style={{ marginRight: "1rem" }}>
-            <h1>Recipes</h1>
-          </Link>
-          <Link to={"/checkout"} style={{ marginRight: "1rem" }}>
-            <h1>Checkout</h1>
-          </Link>
-        </div>
-        <Routes>
-          <Route path="/" element={<Recipes />}></Route>
-          <Route path="/checkout" element={<Checkout />}></Route>
-        </Routes>
-      </div>
-      {modal && <Modal recipe={modal} />}
-    </>
-  )
+  return <div></div>
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -2,28 +2,4 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App.jsx"
 
-import { configureStore } from "@reduxjs/toolkit"
-import { Provider } from "react-redux"
-import { BrowserRouter } from "react-router-dom"
-
-import recipesReducer from "./Slices/recipeSlice.js"
-import modalReducer from "./Slices/modalSlice.js"
-import checkoutReducer from "./Slices/checkoutSlice.js"
-import onlyEasyReducer from "./Slices/onlyEasySlice.js"
-
-const store = configureStore({
-  reducer: {
-    recipes: recipesReducer,
-    modal: modalReducer,
-    checkout: checkoutReducer,
-    onlyEasy: onlyEasyReducer,
-  },
-})
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </BrowserRouter>
-)
+ReactDOM.createRoot(document.getElementById("root")).render(<App />)
