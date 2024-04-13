aedbe95515b8e661cecc7b868dd20d902f428476
Hamza Mirza <hamza665@hotmail.de>
2024-04-13 15:40:10 +0200

    checkout functionality

--- src/Pages/Checkout.jsx
+++ src/Pages/Checkout.jsx
@@ -4,12 +4,16 @@ import RecipeDiv from "../Components/RecipeDiv"
 
 const Checkout = () => {
   const checkout = useSelector((state) => state.checkout.value)
+  const totalPrepTime = checkout.reduce((prev, curr) => {
+    return prev + curr.prepTimeMinutes
+  }, 0)
 
   return (
     <div>
       {checkout.map((item) => (
         <RecipeDiv key={item.id} recipe={item} />
       ))}
+      <p>Your total prep time: {totalPrepTime}</p>
     </div>
   )
 }
