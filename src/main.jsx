472a10a8be3982180a761aa05fd040d26d2bfee0
Hamza Mirza <hamza665@hotmail.de>
2024-04-27 13:24:21 +0200

    deleted files

--- src/main.jsx
+++ src/main.jsx
@@ -2,28 +2,4 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App.jsx"
 
-import { configureStore } from "@reduxjs/toolkit"
-import { Provider } from "react-redux"
-import { BrowserRouter } from "react-router-dom"
-
-import recipesReducer from "./Slices/recipesSlice.js"
-import changeRecipesReducer from "./Slices/changeRecipesSlice.js"
-import modalReducer from "./Slices/modalSlice.js"
-import checkoutReducer from "./Slices/checkoutSlice.js"
-
-const store = configureStore({
-  reducer: {
-    recipes: recipesReducer,
-    changeRecipes: changeRecipesReducer,
-    modal: modalReducer,
-    checkout: checkoutReducer,
-  },
-})
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </BrowserRouter>
-)
+ReactDOM.createRoot(document.getElementById("root")).render(<App />)
