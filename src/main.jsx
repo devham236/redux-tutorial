2a936688b4f74f4a35d51bdb25fd16944e1ddab0
Hamza Mirza <hamza665@hotmail.de>
2024-05-12 09:29:24 +0200

    setup

--- src/main.jsx
+++ src/main.jsx
@@ -2,5 +2,18 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App.jsx"
 import "./index.css"
+import { BrowserRouter } from "react-router-dom"
+import { configureStore } from "@reduxjs/toolkit"
+import { Provider } from "react-redux"
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />)
+const store = configureStore({
+  reducer: {},
+})
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+)
